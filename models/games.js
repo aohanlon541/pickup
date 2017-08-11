7d601ccb3dd9f13dc1d7c02d34713bbf45f4d021
Gregory Mariuzza <Gregory Mariuzza>
2017-08-10 19:13:46 -0500

    updated models

---
+++ models/games.js
@@ -0,0 +1,23 @@
+module.exports = function(sequelize, DataTypes) {
+  var Games = sequelize.define("games", {
+    location: {
+      type: Sequelize.STRING, allowNull: false
+    },
+    sport: {
+      type: Sequelize.STRING, allowNull: false
+    },
+    active: {
+      type: Sequelize.BOOLEAN, allowNull: false
+    },
+    maxNumPlayers: {
+      type: Sequelize.INTEGER, allowNull: false,
+    },
+    start: {
+      type: Sequelize.DATE
+    },
+    end: {
+      type: Sequelize.DATE
+    }
+  });
+  return Games;
+};
